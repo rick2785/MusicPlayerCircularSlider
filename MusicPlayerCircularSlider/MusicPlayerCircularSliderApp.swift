2166ea56e2b0014646a6eab0e503bbd04f290162
rick2785 <rjhrabowskie@gmail.com>
2020-09-25 15:33:13 -0500

    Initial Commit

---
+++ MusicPlayerCircularSlider/MusicPlayerCircularSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerCircularSliderApp.swift
+//  MusicPlayerCircularSlider
+//
+//  Created by RJ Hrabowskie on 9/25/20.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerCircularSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
