cc3c366280630960180e2c4e64fb73b9b95fc07d
rick2785 <rjhrabowskie@gmail.com>
2020-09-25 19:57:28 -0500

    Custom Music Player With Circular Slider And Volume Control using SwiftUI 2.0

---
+++ MusicPlayerCircularSlider/Model/Album.swift
@@ -0,0 +1,15 @@
+//
+//  Album.swift
+//  MusicPlayerCircularSlider
+//
+//  Created by RJ Hrabowskie on 9/25/20.
+//
+
+import SwiftUI
+
+struct Album {
+    var title: String = ""
+    var artist: String = ""
+    var type: String = ""
+    var artwork: UIImage = UIImage(named: "music")!
+}
